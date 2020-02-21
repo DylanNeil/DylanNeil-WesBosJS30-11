d1798ada653dd2af77bf01265691ff2c89738c6f
Dylan Neil <dpagillo@gmail.com>
2020-02-20 20:44:51 -0500

    added play button

--- scripts.js
+++ scripts.js
@@ -7,14 +7,22 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+//Build out functions
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+  console.log('update the button');
+}
 
 // Hook up the event listeners
 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
